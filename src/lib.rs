7a56c7487344a02671d5aaaedb104d01f3efe3a6
Raj pop <rajmanishsahu1121@gmail.com>
2026-01-22 22:12:34 +0530

    completed Strsplit

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,9 @@ where
     fn next(&mut self) -> Option<Self::Item> {
         let remainder = self.remainder.as_mut()?;
         if let Some((delim_start, delim_end)) = self.delimeter.find_next(remainder) {
+            if delim_start == delim_end {
+                return self.remainder.take();
+            }
             let until_delimeter = &remainder[..delim_start];
             *remainder = &remainder[delim_end..];
             Some(until_delimeter)
@@ -35,8 +38,45 @@ where
 
 impl Delimeter for &str {
     fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        if self.is_empty() {
+            return None;
+        }
         s.find(self).map(|start| (start, start + self.len()))
     }
 }
 
-impl Delimeter for char {}
+impl Delimeter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices()
+            .find(|(_, c)| c == self)
+            .map(|(start, _)| (start, start + self.len_utf8()))
+    }
+}
+
+#[test]
+fn for_no_delim() {
+    let haystack = "a b c d e";
+    let letters: Vec<_> = StrSplit::new(haystack, "").collect();
+    assert_eq!(letters, vec!["a b c d e"]);
+}
+
+#[test]
+fn for_one_delim() {
+    let haystack = "a b c d e";
+    let letters: Vec<_> = StrSplit::new(haystack, "d").collect();
+    assert_eq!(letters, vec!["a b c ", " e"]);
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
+}
